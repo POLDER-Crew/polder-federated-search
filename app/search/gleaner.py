66cdffdbe71e23b7b76c6d590d279e8534ca319a
melinda <melinda@melindaminch.com>
2022-01-27 13:10:41 -0800

    DRY up gleaner query code

--- app/search/gleaner.py
+++ app/search/gleaner.py
@@ -3,65 +3,96 @@ from .search import SearcherBase, SearchResultSet, SearchResult
 
 
 class GleanerSearch(SearcherBase):
+    @staticmethod
+    def build_query(user_query=""):
+        return f"""
+            PREFIX sschema: <https://schema.org/>
+            PREFIX schema: <http://schema.org/>
+
+            SELECT
+                (MAX(?relevance) AS ?score)
+                ?id
+                ?url
+                ?title
+                (GROUP_CONCAT(DISTINCT ?abstract ; separator=", ") as ?abstract)
+                (GROUP_CONCAT(DISTINCT ?sameAs ; separator=", ") as ?sameAs)
+                (GROUP_CONCAT(DISTINCT ?keywords ; separator=", ") as ?keywords)
+                (GROUP_CONCAT(DISTINCT ?temporal_coverage ; separator=", ") as ?temporal_coverage)
+
+            {{
+                VALUES ?type {{ schema:Dataset sschema:Dataset }}
+                VALUES ?ids {{ schema:identifier sschema:identifier }}
+                VALUES ?urls {{ sschema:url schema:url }}
+                VALUES ?titles {{ sschema:name schema:name }}
+                VALUES ?abstracts {{ sschema:description schema:description }}
+                VALUES ?keys {{ sschema:keywords schema:keywords }}
+                VALUES ?sameAsVals {{ sschema:sameAs schema:sameAs }}
+                VALUES ?temporal {{ sschema:temporalCoverage schema:temporalCoverage }}
+
+                BIND(
+                    IF(
+                        CONTAINS(?temporal_cov, "/"),
+                        STRDT(STRBEFORE(?temporal_cov, "/"), xsd:date),
+                        IF(
+                            CONTAINS(?temporal_cov, " - "),
+                            STRDT(STRBEFORE(?temporal_cov, " - "), xsd:date),
+                            ?temporal_cov
+                        )
+                    )
+                  AS ?start_date
+                )
+
+                BIND(
+                    IF(
+                        CONTAINS(?temporal_cov, "/"),
+                        STRDT(STRAFTER(?temporal_cov, "/"), xsd:date),
+                        IF(
+                            CONTAINS(?temporal_cov, " - "),
+                            STRDT(STRAFTER(?temporal_cov, " - "), xsd:date),
+                            ?temporal_cov
+                        )
+                    )
+                  AS ?end_date
+                )
+
+                ?s a ?type .
+
+                {user_query}
+
+                ?s ?ids ?id .
+                ?s ?urls ?url .
+                ?s ?titles ?title .
+                OPTIONAL {{
+                    ?s ?abstracts ?abstract .
+                    ?s ?keys ?keyword .
+                    ?s ?sameAsVals ?sameAs .
+                    ?s ?temporal ?temporal_cov .
+                }}
+
+            }}
+            GROUP BY ?id ?url ?title
+            ORDER BY DESC(?score)
+            OFFSET 0
+            LIMIT {GleanerSearch.PAGE_SIZE}
+        """
 
     def __init__(self, **kwargs):
         ENDPOINT_URL = kwargs.pop('endpoint_url')
         self.sparql = SPARQLWrapper(ENDPOINT_URL)
 
     def text_search(self, text=None):
-        # Assigning this to a class member makes it easier to test
-        self.query = f"""
-                PREFIX sschema: <https://schema.org/>
-                PREFIX schema: <http://schema.org/>
-
-                SELECT
-                    (MAX(?relevance) AS ?score)
-                    ?id
-                    ?url
-                    ?sameAs
-                    ?title
-                    ?abstract
-                    (GROUP_CONCAT(DISTINCT ?keyword ; separator=", ") as ?keywords)
-                    (GROUP_CONCAT(DISTINCT ?temporal_cov ; separator=", ") as ?temporal_coverage)
-
-                {{
-                  VALUES ?type {{ schema:Dataset sschema:Dataset }}
-                  VALUES ?ids {{ schema:identifier sschema:identifier }}
-                  VALUES ?urls {{ sschema:url schema:url }}
-                  VALUES ?titles {{ sschema:name schema:name }}
-                  VALUES ?abstracts {{ sschema:description schema:description }}
-                  VALUES ?keys {{ sschema:keywords schema:keywords }}
-                  VALUES ?sameAsVals {{ sschema:sameAs schema:sameAs }}
-                  VALUES ?temporal {{ sschema:temporalCoverage schema:temporalCoverage }}
-
-                  ?s a ?type .
-
-                  ?lit bds:search "{text}" .
-                  ?lit bds:matchAllTerms "false" .
-                  ?lit bds:relevance ?relevance .
-                  ?s ?p ?lit .
-
-                  graph ?g {{
-                    ?s ?ids ?id .
-                    ?s ?urls ?url .
-                    ?s ?titles ?title .
-                    Optional {{
-                        ?s ?abstracts ?abstract .
-                        ?s ?keys ?keyword .
-                        ?s ?sameAsVals ?sameAs .
-                        ?s ?temporal ?temporal_cov .
-                    }}
-                  }}
-
-                }}
-                GROUP BY ?id ?url ?sameAs ?title ?abstract
-                ORDER BY DESC(?score)
-                OFFSET 0
-            LIMIT {self.PAGE_SIZE}
+        user_query = f"""
+            ?lit bds:search "{text}" .
+            ?lit bds:matchAllTerms "false" .
+            ?lit bds:relevance ?relevance .
+            ?s ?p ?lit .
         """
-        self.sparql.setQuery(self.query)
-        # a note: BlazeGraph relevance scores go from 0.0 to 1.0; all results are normalized.
 
+        # Assigning this to a class member makes it easier to test
+        self.query = GleanerSearch.build_query(user_query)
+        self.sparql.setQuery(self.query)
+
+        # a note: BlazeGraph relevance scores go from 0.0 to 1.0; all results are normalized.
         self.sparql.setReturnFormat(JSON)
         data = self.sparql.query().convert()
         result_set = SearchResultSet(
@@ -83,79 +114,9 @@ class GleanerSearch(SearcherBase):
             date_filter += f"FILTER(?end_date <= '{end_max.isoformat()}'^^xsd:date)"
 
 
-        self.query = f"""
-                PREFIX sschema: <https://schema.org/>
-                PREFIX schema: <http://schema.org/>
+        # Assigning this to a class member makes it easier to test
+        self.query = GleanerSearch.build_query(date_filter)
 
-                SELECT
-                    ?id
-                    ?url
-                    ?sameAs
-                    ?title
-                    ?abstract
-                    (GROUP_CONCAT(DISTINCT ?keyword ; separator=", ") AS ?keywords)
-                    (GROUP_CONCAT(DISTINCT ?temporal_cov ; separator=", ") AS ?temporal_coverage)
-                    ?start_date
-                    ?end_date
-
-                    {{
-
-                        VALUES ?type {{ schema:Dataset sschema:Dataset }}
-                        VALUES ?ids {{ schema:identifier sschema:identifier }}
-                        VALUES ?urls {{ sschema:url schema:url }}
-                        VALUES ?titles {{ sschema:name schema:name }}
-                        VALUES ?abstracts {{ sschema:description schema:description }}
-                        VALUES ?keys {{ sschema:keywords schema:keywords }}
-                        VALUES ?sameAsVals {{ sschema:sameAs schema:sameAs }}
-                        VALUES ?temporal {{ sschema:temporalCoverage schema:temporalCoverage }}
-
-                        BIND(
-                            IF(
-                                CONTAINS(?temporal_cov, "/"),
-                                STRDT(STRBEFORE(?temporal_cov, "/"), xsd:date),
-                                IF(
-                                    CONTAINS(?temporal_cov, " - "),
-                                    STRDT(STRBEFORE(?temporal_cov, " - "), xsd:date),
-                                    ?temporal_cov
-                                )
-                            )
-                          AS ?start_date
-                        )
-
-                        BIND(
-                            IF(
-                                CONTAINS(?temporal_cov, "/"),
-                                STRDT(STRAFTER(?temporal_cov, "/"), xsd:date),
-                                IF(
-                                    CONTAINS(?temporal_cov, " - "),
-                                    STRDT(STRAFTER(?temporal_cov, " - "), xsd:date),
-                                    ?temporal_cov
-                                )
-                            )
-                          AS ?end_date
-                        )
-
-
-                          ?s a ?type .
-
-                          {date_filter}
-
-                          ?s ?ids ?id .
-                          ?s ?urls ?url .
-                          ?s ?temporal ?temporal_cov .
-                          ?s ?titles ?title .
-                          OPTIONAL {{
-                            ?s ?abstracts ?abstract .
-                            ?s ?keys ?keyword .
-                            ?s ?sameAsVals ?sameAs .
-                          }}
-                    }}
-
-                }}
-            GROUP BY ?id ?url ?sameAs ?title ?abstract ?start_date ?end_date
-            OFFSET 0
-            LIMIT {self.PAGE_SIZE}
-        """
         self.sparql.setQuery(self.query)
         self.sparql.setReturnFormat(JSON)
         data = self.sparql.query().convert()
