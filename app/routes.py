db13b5486cdc1fab2c757e652dce45f7fb77f75c
melinda <melinda@melindaminch.com>
2022-02-03 11:38:59 -0800

    Make the search route do a combined search

--- app/routes.py
+++ app/routes.py
@@ -10,21 +10,32 @@ from app.search.search import SearchResultSet
 def home():
     return render_template('index.html')
 
-def _do_text_search(text, template):
-    dataone = SolrDirectSearch().text_search(text)
-    gleaner = GleanerSearch(endpoint_url=app.config['GLEANER_ENDPOINT_URL']).text_search(text)
+
+def _do_combined_search(template, **kwargs):
+    text = kwargs.pop('text', None)
+
+    # These all need to be date objects
+    start_min = kwargs.pop('start_min', None)
+    start_max = kwargs.pop('start_max', None)
+    end_min = kwargs.pop('end_min', None)
+    end_max = kwargs.pop('end_max', None)
+
+    dataone = SolrDirectSearch().combined_search(
+        text, start_min, start_max, end_min, end_max)
+    gleaner = GleanerSearch(endpoint_url=app.config['GLEANER_ENDPOINT_URL']).combined_search(
+        text, start_min, start_max, end_min, end_max)
 
     results = SearchResultSet.collate(dataone, gleaner)
+
     return render_template(template, result_set=results)
+
 
 @app.route('/search')
 # Redirect to a stand-alone page to display results
-def nojs_text_search():
-    text = request.args.get('text')
-    return _do_text_search(text, 'search.html')
+def nojs_combined_search():
+    return _do_combined_search('search.html', **request.args)
 
 
 @app.route('/api/search')
-def text_search():
-    text = request.args.get('text')
-    return _do_text_search(text, 'results.html')
+def combined_search():
+    return _do_combined_search('results.html', **request.args)
