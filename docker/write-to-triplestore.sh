b5567335e9c3795deb8bf3b185dbcda46bf7bb8f
melinda <melinda@melindaminch.com>
2022-04-22 15:33:35 -0700

    Experiments

--- docker/write-to-triplestore.sh
+++ docker/write-to-triplestore.sh
@@ -2,5 +2,5 @@
 
 mc config host add minio http://localhost:9000
 for i in $(mc find minio/gleaner/milled); do
-  mc cat $i | curl -X POST -H 'Content-Type:text/rdf+n3;charset=utf-8' --data-binary  @- http://localhost:9999/bigdata/namespace/polder/sparql
+  mc cat <(echo 'update=insert data {') $i <(echo '}') | curl -X POST -H 'Content-Type:application/sparql-update;charset=utf-8' -u "admin:admin" --data-binary @- 'http://localhost:9999/polder/update'
 done
