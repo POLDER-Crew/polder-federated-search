a43ff5d47d51560d6923ab4030a07822704bf383
melinda <melinda@melindaminch.com>
2022-07-04 15:35:44 -0700

    Set up repository in docker

--- docker/clear-triplestore.sh
+++ docker/clear-triplestore.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
+# Delete the repository
 curl --get -X DELETE http://localhost:9999/rest/repositories/polder
-
+# Create the repository
+curl -X POST -H 'Content-Type:multipart/form-data' -F "config=@./polder.ttl" http://localhost:9999/rest/repositories
+# Recreate Lucene and geosparql connectors
+curl -X POST --header 'Accept: application/json' --data-urlencode update@./lucene-connector.sparql http://localhost:9999/repositories/polder/statements
+curl -X POST --header 'Accept: application/json' --data-urlencode update@./geosparql.sparql http://localhost:9999/repositories/polder/statements
---
+++ docker/geosparql.sparql
@@ -0,0 +1,5 @@
+PREFIX geosparql: <http://www.ontotext.com/plugins/geosparql#>
+
+INSERT DATA {
+  [] geosparql:enabled "true" .
+}
