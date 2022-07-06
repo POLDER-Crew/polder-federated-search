6d03ce20395a53de7eaf8afedee6f3dd7647ae77
yemoski <yemisiogungbemi89@gmail.com>
2022-07-06 14:50:49 -0700

    a better sitemap

--- docker/index_gcmd.py
+++ docker/index_gcmd.py
@@ -1,20 +1,25 @@
 from urllib.request import urlopen
 from xml.dom import minidom
-import xml.etree.cElementTree as ET
 
+doc = minidom.Document()
+root = doc.createElement("urlset")
+root.setAttribute("xmlns"  , "http://www.sitemaps.org/schemas/sitemap/0.9")
+root.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+root.setAttribute("xsi:schemalocation"  , "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd")
 
-root = ET.Element('root')
+doc.appendChild(root)
+
 for i in range(1,8):
     url = 'https://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDhttps://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDIS/NCEI&data_center=ASF&data_center=NASA%20NSIDC%20DAAC&data_center=NO/NMDC/IMR&data_center=UK/NERC/POL/PSMSL&data_center=WDC/GEOMAGNETISM,%20EDINBURGH&data_center=WDC/SEP,%20MOSCOWIS/NCEI&data_center=NASA%20NSIDC%20DAAC&page_num='+str(i)+'&page_size=2000'
     mydoc = minidom.parse(urlopen(url))
     items = mydoc.getElementsByTagName('location')
     for i in items:
-    	element = i.firstChild.data
-    	print(element)
-    	ET.SubElement(root,'location').text = element
+        element = doc.createElement("url")
+        element2 = doc.createElement("loc")
+        element2.appendChild(doc.createTextNode(i.firstChild.data))
+        element.appendChild(element2)
+        root.appendChild(element)
 
 
 
-tree = ET.ElementTree(root)
-tree.write('gcmd-sitemap.xml')
-
+doc.writexml(open('gcmd-sitemap.xml','w'),encoding='UTF-8')
