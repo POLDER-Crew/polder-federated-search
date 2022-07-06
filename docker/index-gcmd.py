bb397ecba8b9449e843f637ed2e0fd4bc975f632
yemoski <yemisiogungbemi89@gmail.com>
2022-07-06 16:07:48 -0700

    amount_of_pages fixed 2

--- docker/index-gcmd.py
+++ docker/index-gcmd.py
@@ -8,8 +8,9 @@ root.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
 root.setAttribute("xsi:schemalocation"  , "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd")
 
 doc.appendChild(root)
+query = 'https://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDhttps://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDIS/NCEI&data_center=ASF&data_center=NASA%20NSIDC%20DAAC&data_center=NO/NMDC/IMR&data_center=UK/NERC/POL/PSMSL&data_center=WDC/GEOMAGNETISM,%20EDINBURGH&data_center=WDC/SEP,%20MOSCOWIS/NCEI&data_center=NASA%20NSIDC%20DAAC&page_num={page:d}&page_size=2000'
 
-baseurl = 'https://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDhttps://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDIS/NCEI&data_center=ASF&data_center=NASA%20NSIDC%20DAAC&data_center=NO/NMDC/IMR&data_center=UK/NERC/POL/PSMSL&data_center=WDC/GEOMAGNETISM,%20EDINBURGH&data_center=WDC/SEP,%20MOSCOWIS/NCEI&data_center=NASA%20NSIDC%20DAAC&page_num=1&page_size=2000'
+baseurl = query.format(page=1)
 
 mybasedoc = minidom.parse(urlopen(baseurl))
 hits = mybasedoc.getElementsByTagName('hits')
@@ -17,7 +18,7 @@ hits = mybasedoc.getElementsByTagName('hits')
 pagecount = int(hits[0].firstChild.nodeValue)
 amount_of_pages = int (pagecount/2000) + 2
 for i in range(1,amount_of_pages):
-    url = 'https://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDhttps://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDIS/NCEI&data_center=ASF&data_center=NASA%20NSIDC%20DAAC&data_center=NO/NMDC/IMR&data_center=UK/NERC/POL/PSMSL&data_center=WDC/GEOMAGNETISM,%20EDINBURGH&data_center=WDC/SEP,%20MOSCOWIS/NCEI&data_center=NASA%20NSIDC%20DAAC&page_num='+str(i)+'&page_size=2000'
+    url = query.format(page=i)
     mydoc = minidom.parse(urlopen(url))
     items = mydoc.getElementsByTagName('location')
     for i in items:
