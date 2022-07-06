946ee4757b70ee6c69f295e7061c5ba84c2cedb6
yemoski <yemisiogungbemi89@gmail.com>
2022-07-06 15:51:13 -0700

    amount_of_pages fixed

--- docker/index-gcmd.py
+++ docker/index-gcmd.py
@@ -9,7 +9,14 @@ root.setAttribute("xsi:schemalocation"  , "http://www.sitemaps.org/schemas/sitem
 
 doc.appendChild(root)
 
-for i in range(1,8):
+baseurl = 'https://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDhttps://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDIS/NCEI&data_center=ASF&data_center=NASA%20NSIDC%20DAAC&data_center=NO/NMDC/IMR&data_center=UK/NERC/POL/PSMSL&data_center=WDC/GEOMAGNETISM,%20EDINBURGH&data_center=WDC/SEP,%20MOSCOWIS/NCEI&data_center=NASA%20NSIDC%20DAAC&page_num=1&page_size=2000'
+
+mybasedoc = minidom.parse(urlopen(baseurl))
+hits = mybasedoc.getElementsByTagName('hits')
+
+pagecount = int(hits[0].firstChild.nodeValue)
+amount_of_pages = int (pagecount/2000) + 2
+for i in range(1,amount_of_pages):
     url = 'https://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDhttps://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDIS/NCEI&data_center=ASF&data_center=NASA%20NSIDC%20DAAC&data_center=NO/NMDC/IMR&data_center=UK/NERC/POL/PSMSL&data_center=WDC/GEOMAGNETISM,%20EDINBURGH&data_center=WDC/SEP,%20MOSCOWIS/NCEI&data_center=NASA%20NSIDC%20DAAC&page_num='+str(i)+'&page_size=2000'
     mydoc = minidom.parse(urlopen(url))
     items = mydoc.getElementsByTagName('location')
