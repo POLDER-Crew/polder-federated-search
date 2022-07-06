5a8ab1fbf6d254cf00ec43a22055d29aaff92da2
yemoski <yemisiogungbemi89@gmail.com>
2022-07-06 16:18:22 -0700

    amount_of_pages fixed 2

--- docker/index-gcmd.py
+++ docker/index-gcmd.py
@@ -10,14 +10,25 @@ root.setAttribute("xsi:schemalocation"  , "http://www.sitemaps.org/schemas/sitem
 doc.appendChild(root)
 query = 'https://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDhttps://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDIS/NCEI&data_center=ASF&data_center=NASA%20NSIDC%20DAAC&data_center=NO/NMDC/IMR&data_center=UK/NERC/POL/PSMSL&data_center=WDC/GEOMAGNETISM,%20EDINBURGH&data_center=WDC/SEP,%20MOSCOWIS/NCEI&data_center=NASA%20NSIDC%20DAAC&page_num={page:d}&page_size=2000'
 
+# data on page 1
 baseurl = query.format(page=1)
-
 mybasedoc = minidom.parse(urlopen(baseurl))
 hits = mybasedoc.getElementsByTagName('hits')
+items_page_1 = mybasedoc.getElementsByTagName('location')
+for i in items_page_1:
+        element = doc.createElement("url")
+        element2 = doc.createElement("loc")
+        element2.appendChild(doc.createTextNode(i.firstChild.data))
+        element.appendChild(element2)
+        root.appendChild(element)
+
+
 
 pagecount = int(hits[0].firstChild.nodeValue)
 amount_of_pages = int (pagecount/2000) + 2
-for i in range(1,amount_of_pages):
+
+# data from page 2 onwards
+for i in range(2,amount_of_pages):
     url = query.format(page=i)
     mydoc = minidom.parse(urlopen(url))
     items = mydoc.getElementsByTagName('location')
