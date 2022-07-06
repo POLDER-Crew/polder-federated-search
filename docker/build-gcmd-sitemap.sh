35d19086a1ce3d567cd996d40afe787ec0d6cf81
yemoski <yemisiogungbemi89@gmail.com>
2022-07-05 19:01:24 -0700

    built-gcmd sitemap

---
+++ docker/build-gcmd-sitemap.sh
@@ -0,0 +1,7 @@
+
+python index_gcmd.py
+#for i in {1..500}
+#do 
+#curl -i "https://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDhttps://cmr.earthdata.nasa.gov/search/collections?data_center=AU/AADC&data_center=WGMS&data_center=DOC/NOAA/NESDIS/NCEI&data_center=ASF&data_center=NASA%20NSIDC%20DAAC&data_center=NO/NMDC/IMR&data_center=UK/NERC/POL/PSMSL&data_center=WDC/GEOMAGNETISM,%20EDINBURGH&data_center=WDC/SEP,%20MOSCOWIS/NCEI&data_center=NASA%20NSIDC%20DAAC&page_num=${i}&page_size=2000"
+
+#done
