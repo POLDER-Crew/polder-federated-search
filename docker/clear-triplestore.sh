f9328de264fbeba486f6bd0937300d07287be645
melinda <melinda@melindaminch.com>
2022-07-07 16:02:35 -0700

    Fix reindex task

--- docker/clear-triplestore.sh
+++ docker/clear-triplestore.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 # Delete the repository
-curl --get -X DELETE http://localhost:9999/rest/repositories/polder
+curl -vs --get -X DELETE http://localhost:9999/rest/repositories/polder
 # Create the repository
-curl -X POST -H 'Content-Type:multipart/form-data' -F "config=@./polder.ttl" http://localhost:9999/rest/repositories
+curl -vs -X POST -H 'Content-Type:multipart/form-data' -F "config=@./polder.ttl" http://localhost:9999/rest/repositories
 # Recreate Lucene and geosparql connectors
-curl -X POST --header 'Accept: application/json' --data-urlencode update@./lucene-connector.sparql http://localhost:9999/repositories/polder/statements
-curl -X POST --header 'Accept: application/json' --data-urlencode update@./geosparql.sparql http://localhost:9999/repositories/polder/statements
+curl -vs -X POST --header 'Accept: application/json' --data-urlencode update@./lucene-connector.sparql http://localhost:9999/repositories/polder/statements
+curl -vs -X POST --header 'Accept: application/json' --data-urlencode update@./geosparql.sparql http://localhost:9999/repositories/polder/statements
