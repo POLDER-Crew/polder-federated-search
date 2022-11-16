fa2fb8000e1a039f2de6c2f21f2e767f99077448
yemoski <yemisiogungbemi89@gmail.com>
2022-11-16 15:36:02 -0800

    typo_removed

--- build-sitemap/index-datastream.py
+++ build-sitemap/index-datastream.py
@@ -5,8 +5,8 @@ import pandas as pd
 import datetime
 from requests.structures import CaseInsensitiveDict
 
-# Make a get request to get the latest player data from the FPL API
-#link = "https://api.datastream.org/v1/odata/v4/Metadata?$top=1000"
+# Make a get request to get Metadata info from Datastream
+
 link = "https://api.datastream.org/v1/odata/v4/Metadata?$top=10000&$filter=RegionId eq 'hub.atlantic' or RegionId eq 'hub.lakewinnipeg' or RegionId eq 'hub.mackenzie'"
 api_key = '3SKMA34iPB7LLcnweqd3IC7MdGXZRTNr'
 headers = CaseInsensitiveDict()
