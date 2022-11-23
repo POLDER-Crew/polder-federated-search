e4d70378e57694d5245d7fb62a7f0444953f71f1
melinda <melindaminch@oceannetworks.ca>
2022-11-23 15:47:15 -0800

    Add scripts to build a sitemap for CCADI

--- build-sitemap/build-sitemaps.sh
+++ build-sitemap/build-sitemaps.sh
@@ -47,3 +47,18 @@ mc cp bas-sitemap.xml minio/sitemaps/bas-sitemap.xml
 python3 /usr/local/bin/index-gcmd.py
 
 mc cp gcmd-sitemap.xml minio/sitemaps/gcmd-sitemap.xml
+
+# Build the CCADI sitemap
+
+rm -f ccadi-sitemap.xml
+
+# build the skeleton of the XML sitemap
+printf '<?xml version="1.0" encoding="UTF-8"?>\n' >> ccadi-sitemap.xml
+printf '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemalocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' >> bas-sitemap.xml
+for i in http://hedeby.uwaterloo.ca/api/metadata?page={0..150}
+    do printf "<url><loc>${i}</loc></url>"
+done
+
+printf "</urlset>" >> ccadi-sitemap.xml
+
+mc cp ccadi-sitemap.xml minio/sitemaps/ccadi-sitemap.xml
