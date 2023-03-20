51f8ba607ddaeebe1689a93879f3354c08c1ba3c
melinda <melindaminch@oceannetworks.ca>
2023-03-20 16:40:24 -0700

    Make triplestore write script recover from connection and auth token losses

--- deployment-support/graphdb/write-to-triplestore.sh
+++ deployment-support/graphdb/write-to-triplestore.sh
@@ -1,7 +1,27 @@
 #! /bin/bash
-# Get the security token to work with graphdb
-token=$(curl -X POST -i -H "X-GraphDB-Password: $GRAPHDB_INDEXER_PASSWORD" -H 'Content-type: application/json' "$GRAPHDB_REST_URL"/login/"$GRAPHDB_INDEXER_USER" | awk 'BEGIN {FS=": "}/^Authorization/{gsub(/\r/,"",$0); print $2}')
 mc config host add minio "$MINIO_SERVER_HOST":"$MINIO_SERVER_PORT" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
+
+token=$(curl -X POST -i -H "X-GraphDB-Password: $GRAPHDB_INDEXER_PASSWORD" -H 'Content-type: application/json' "$GRAPHDB_REST_URL"/login/"$GRAPHDB_INDEXER_USER" | awk 'BEGIN {FS=": "}/^Authorization/{gsub(/\r/,"",$0); print $2}')
+
 for i in $(mc find minio/"$STORAGE_BUCKET"/milled); do
-  mc cat $i | curl -X POST -H "Authorization:$token" -H 'Content-Type:text/rdf+n3;charset=utf-8' --data-binary  @- "$GLEANER_ENDPOINT_URL"/statements
+  # Get the security token to work with graphdb
+  status=$(mc cat $i | curl -o /dev/null -s -w '%{http_code}' -X POST -H "Authorization:$token" -H 'Content-Type:text/rdf+n3;charset=utf-8' --data-binary  @- "$GLEANER_ENDPOINT_URL"/statements)
+  # status 000 often leads to subsequent 401, which is why these statements are in this order.
+  if [ "$status" = "000" ]
+  then
+    while [ "$status" = "000" ]
+    do
+      echo "failed to connect to minio. Retrying in 10 seconds..."
+      sleep 10
+      status=$(mc cat $i | curl -s -w '%{http_code}' -X POST -H "Authorization:$token" -H 'Content-Type:text/rdf+n3;charset=utf-8' --data-binary  @- "$GLEANER_ENDPOINT_URL"/statements)
+    done
+  fi
+  if [ "$status" = "401" ]
+  then
+    echo "Authentication failed while writing." $i "Fetching new token and retrying."
+    token=$(curl -X POST -i -H "X-GraphDB-Password: $GRAPHDB_INDEXER_PASSWORD" -H 'Content-type: application/json' "$GRAPHDB_REST_URL"/login/"$GRAPHDB_INDEXER_USER" | awk 'BEGIN {FS=": "}/^Authorization/{gsub(/\r/,"",$0); print $2}')
+    status=$(mc cat $i | curl -s -w '%{http_code}' -X POST -H "Authorization:$token" -H 'Content-Type:text/rdf+n3;charset=utf-8' --data-binary  @- "$GLEANER_ENDPOINT_URL"/statements)
+  fi
+  echo "RDF upload status for" $i ":" $status
+
 done
